719227141d2ac1098880d5f75c0367f5f92b9d51
admluansilva95-oss <adm.luansilva95@gmail.com>
2026-05-22 18:24:51 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,19 @@
 import './globals.css';
 
 export const metadata = {
-  title: 'Meu Cardápio Virtual',
-  description: 'Venha conferir nossas delícias!',
+  title: 'Nome do Restaurante - Cardápio Virtual',
+  description: 'Venha conferir nossas delícias e faça seu pedido!',
+  openGraph: {
+    title: 'Nome do Restaurante - Cardápio Virtual',
+    description: 'Venha conferir nossas delícias e faça seu pedido!',
+    images: [
+      {
+        url: 'COLE_AQUI_O_LINK_DA_SUA_FOTO.jpg', // Coloque o link da foto aqui
+        width: 1200,
+        height: 630,
+        alt: 'Capa do Cardápio',
+      },
+    ],
+  },
 }
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="pt-BR">
-      <body>{children}</body>
-    </html>
-  )
-}
+  
