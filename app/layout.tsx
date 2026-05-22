ab7f825eb87019b01b193d1083bff9f478aa4afb
admluansilva95-oss <adm.luansilva95@gmail.com>
2026-05-22 18:47:01 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ export const metadata = {
     description: 'Venha conferir nossas delícias e faça seu pedido!',
     images: [
       {
-        url: 'https://celeitoroast.netlify.app/opengraph-image.png',
+        url: 'https://celeitoroast.netlify.app/opengraph-image.png', // Se sua foto for JPG, mude o final para .jpg
         width: 1200,
         height: 630,
         alt: 'Logo Celeiro Roast',
