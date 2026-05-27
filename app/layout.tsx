2baee82d22301e1df9f281bcf51232c19f95bce2
admluansilva95-oss <adm.luansilva95@gmail.com>
2026-05-27 12:53:49 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,11 @@
-import './globals.css';
+import "./globals.css";
 
 export const metadata = {
-  title: 'Celeiro Roast - Cardápio Virtual',
-  description: 'Venha conferir nossas delícias e faça seu pedido!',
+  title: "Cardápio digital",
+  description: "Cardápio online e pedidos pelo WhatsApp.",
   openGraph: {
-    title: 'Celeiro Roast - Cardápio Virtual',
-    description: 'Venha conferir nossas delícias e faça seu pedido!',
-    images: [
-      {
-        url: 'https://celeitoroast.netlify.app/opengraph-image.png', // Se sua foto for JPG, mude o final para .jpg
-        width: 1200,
-        height: 630,
-        alt: 'Logo Celeiro Roast',
-      },
-    ],
+    title: "Cardápio digital",
+    description: "Cardápio online e pedidos pelo WhatsApp.",
   },
 };
 
